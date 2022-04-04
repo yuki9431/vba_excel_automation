a390f034d83fb50171e5e285af45c5deaff26a2f
Dillen Hiroyuki <yuki9431@gmail.com>
2022-04-04 16:25:03 +0900

    Add files via upload

---
+++ main.vb
@@ -0,0 +1,14 @@
+' Excel | Move to A1 and display zoom 100%
+Sub moveA1_zoom100()
+    Dim cell As Worksheet
+    Dim sheet As Worksheet
+
+    Set sheet = ActiveSheet
+
+    For Each cell In Worksheet
+        cell.Select
+        ActiveSheet.Zoom = 100
+        cell.Range("A1").Select
+    Next
+Sheets(1).Select
+End Sub
