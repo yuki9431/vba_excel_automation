03463420970aa28b620fe86c6fe44f498737bafb
Dillen Hiroyuki <yuki9431@gmail.com>
2022-04-06 15:19:58 +0900

    Fix main.vb
    Remove useless codes. Support for hidden sheets

--- main.vb
+++ main.vb
@@ -1,14 +1,11 @@
 ' Excel | Move to A1 and display zoom 100%
 Sub moveA1_zoom100()
-    Dim cell As Worksheet
     Dim sheet As Worksheet
-
     Set sheet = ActiveSheet
-
-    For Each cell In Worksheet
-        cell.Select
+    For Each sheet In Worksheet
+        sheet.Activate
         ActiveWindow.Zoom = 100
-        cell.Range("A1").Select
+        sheet.Range("A1").Select
     Next
 Sheets(1).Select
 End Sub
