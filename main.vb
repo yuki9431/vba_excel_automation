bd668b97bc8ce9d60311fd71255a592d1569e977
Dillen Hiroyuki <yuki9431@gmail.com>
2022-04-06 15:42:11 +0900

    fix main.vb
    Remove useless codes

--- main.vb
+++ main.vb
@@ -1,11 +1,11 @@
 ' Excel | Move to A1 and display zoom 100%
 Sub moveA1_zoom100()
     Dim sheet As Worksheet
-    Set sheet = ActiveSheet
+
     For Each sheet In Worksheet
         sheet.Activate
+        sheet.Range("A1").Activate
         ActiveWindow.Zoom = 100
-        sheet.Range("A1").Select
     Next
-Sheets(1).Select
+Sheets(1).Activate
 End Sub
